92d38a82d68ee4e3c3a0c07d7cb6a7b565c8f2b9
Charles <c.friend85@yahoo.com>
2021-08-05 00:06:29 -0600

    Algos

---
+++ Algos/diagonal_difference.js
@@ -0,0 +1,10 @@
+function diagonalDifference(arr) {
+    let x = arr.length
+    let main = 0
+    let sec = 0
+    for(let i = 0; i < x; i++){
+        main += arr[i][i]
+        sec += arr[i][x-i-1]
+    }
+    return Math.abs(main - sec)
+}
---
+++ Algos/plus_minus_ratio.js
@@ -0,0 +1,22 @@
+function plusMinus(arr) {
+    let x = arr.length
+    let pos = 0
+    let neg = 0
+    let zero = 0
+    for(let i = 0; i < x; i++){
+        if(arr[i] > 0){
+            pos += 1
+        }
+        else if(arr[i] < 0){
+            neg += 1
+        }
+        else
+            zero += 1
+    }
+    let posRatio = pos / x
+    let negRatio = neg / x
+    let zeroRatio = zero / x
+    console.log(posRatio.toFixed(6))
+    console.log(negRatio.toFixed(6))
+    console.log(zeroRatio.toFixed(6))
+}
