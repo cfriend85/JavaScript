f708249d4d8e5c28ed602f89d4dc2a2407225a2d
Charles <c.friend85@yahoo.com>
2021-08-09 23:12:33 -0600

    Algos

---
+++ Algos/apples_oranges.js
@@ -0,0 +1,27 @@
+// Filter method
+
+const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
+    let appleCount = apples.filter(value => value + a >= s && value + a <= t).length;
+    let orangeCount = oranges.filter(value => value + b >= s && value + b <= t).length;
+    console.log(appleCount)
+    console.log(orangeCount)
+}
+
+
+//  Loop method
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let totalApples = 0;
+    let totalOranges = 0;
+    for (let i = 0; i <= apples.length; i++) {
+        if (a + apples[i] >= s && a + apples[i] <= t) {
+            totalApples += 1;
+        }
+    }
+    for (let i = 0; i <= oranges.length; i++) {
+        if (b + oranges[i] >= s && b + oranges[i] <= t) {
+            totalOranges += 1;
+        }
+    }
+    console.log(totalApples);
+    console.log(totalOranges);
+}
