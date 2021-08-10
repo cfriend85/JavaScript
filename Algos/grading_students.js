46985ebc5d9e865199203d04b523b9c0ab2b4791
Charles <c.friend85@yahoo.com>
2021-08-09 19:40:11 -0600

    Algos

---
+++ Algos/grading_students.js
@@ -0,0 +1,11 @@
+let gradingStudents = (grades) => {
+    for(let i = 0; i < grades.length; i++){
+        if (grades[i] < 38){
+            grades[i] = grades[i]
+        }
+        else if(Math.ceil(grades[i]/5)*5 - grades[i] < 3){
+            grades[i] = Math.ceil(grades[i]/5)*5
+        }
+    }
+    return grades
+}
