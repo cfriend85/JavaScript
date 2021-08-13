498025764fed30af31a9901697c1a8dbbef3646b
Charles <c.friend85@yahoo.com>
2021-08-13 15:51:34 -0600

    HR JS Day 5

--- HR_JS_challenges/day5.js
+++ HR_JS_challenges/day5.js
@@ -6,3 +6,28 @@ const Rectangle = (a, b) => {
     
     return {length, width, perimeter, area} //challenge specified to return as object
 }
+
+const getCount = (objects) => {
+    let count = 0
+    for(let i = 0; i < objects.length; i++){
+        if(objects[i].x - objects[i].y == 0){
+            count += 1
+        }
+    }
+    return count
+}
+
+
+class Polygon {
+    constructor(arr){
+        this.arr = arr
+    }
+    
+    perimeter(){
+        let num = 0
+        for(let i = 0; i < this.arr.length; i++){
+            num += this.arr[i]
+        }
+        return num
+    }
+}
