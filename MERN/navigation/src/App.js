4ce865f46cab79ac7dbe46f7feb43d93b7c3e9d5
Charles <c.friend85@yahoo.com>
2021-08-21 23:58:06 -0600

    Added state

--- MERN/navigation/src/App.js
+++ MERN/navigation/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import Step from './components/Step';
 
 function App() {
+
+  const [steps, setSteps] = useState([
+    {direction: "Right", text:"On Peoria St"},
+    {direction: "Left", text:"On Parker Rd"},
+    {direction: "Forward", text:"On CO 83"},
+    {direction: "Right", text:"On Arapahoe Rd"}
+  ])
+
   return (
     <div className="App">
-      <Step direction="Forward" text="on Oakley Ave"/>
+      <div className="d-flex flex-wrap">
+        {
+      steps.map((item, i) => <Step key={i} direction={item.direction} text={item.text}/>)
+        }
+      </div>
     </div>
   );
 }
--- MERN/navigation/src/components/Step.jsx
+++ MERN/navigation/src/components/Step.jsx
@@ -7,13 +7,13 @@ import forward from '../static/images/forward.svg'
 const Step = (props) => {
     const direction = props.direction
     return(
-    <div className="card">
-        <img src={direction === "Right" ? rightArrow : direction === "Left" ? leftArrow : forward} className="left-arrow" alt="arrow"/>
-    <div className="card-body">
-        <p className="card-text">{direction} {props.text}</p>
-        <a href="#" className="btn btn-danger">Stop Navigation</a>
-    </div>
-</div>
+        <div className="card">
+            <img src={direction === "Right" ? rightArrow : direction === "Left" ? leftArrow : forward} className="left-arrow" alt="arrow"/>
+        <div className="card-body">
+            <p className="card-text">{direction} {props.text}</p>
+            <a href="#" className="btn btn-danger">Stop Navigation</a>
+        </div>
+        </div>
     )
 }
 
