e50be8fa7e7904353ab7ff61b82fd0bed181ca22
Charles <c.friend85@yahoo.com>
2021-08-22 21:52:33 -0600

    Introduce useEffect

--- MERN/api_lecture/src/App.js
+++ MERN/api_lecture/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [breweries, setBreweries] = useState([])
-  const [city, setCity] = useState("Chicago")
-  const [form, setForm] = useState("")
+  const [city, setCity] = useState("Boston")
+
+  useEffect(() => { //this is something that happens as soon as the page or thing it's attached to is instantiated.
+    axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
+    .then(res => setBreweries(res.data))
+    .catch(err => console.log(err))
+  }, [city]) //dependancy array
 
   const getBreweryInfo = (event) => {
     axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}`) // a package that is installed and has an npm page
@@ -24,7 +29,7 @@ function App() {
   }
   return (
     <div className="App">
-      <input type="text" placeholder="City" onChange={(event) =>  setCity(event.target.value)} className="mt-5"/><br></br>
+      <input type="text" placeHolder="City" onChange={(event) =>  setCity(event.target.value)} className="mt-5"/><br></br>
       <button className="btn btn-primary btn-lg mt-5" onClick={getBreweryInfo}>Go!</button>
       {
         breweries.map((item, i) => <h1 key={i}>{item.name}: {item.city}, {item.state}</h1>)
