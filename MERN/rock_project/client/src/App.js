9d5a9b78a270050cb9074f7d8e39a6f0e112085d
Charles <c.friend85@yahoo.com>
2021-08-29 19:46:25 -0600

    Added form and functionality

--- MERN/rock_project/client/src/App.js
+++ MERN/rock_project/client/src/App.js
@@ -6,42 +6,98 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [rocks, setRocks] = useState([])
+const [rocks, setRocks] = useState([])
+const [form, setForm] = useState({
+    "name": "",
+    "color": "",
+    "feel": "",
+    "weight": 0,
+    "foundDate": new Date()
+})
 
-  useEffect(() => {
+const [loaded, setLoaded] = useState(false);
+
+useEffect(() => {
     axios.get("http://localhost:8000/api/rocks")
-      .then(res => setRocks(res.data.rocks))
-      .catch(err => console.log(err))
-  }, [])
-  return (
+    .then(res => {
+		setRocks(res.data.rocks);
+		setLoaded(true);
+	})
+    .catch(err => console.log(err))
+}, [loaded])
+
+const onChangeHandler = (event) => {
+    setForm({
+    ...form,
+    [event.target.name]: event.target.value,
+    })
+}
+
+const onSubmitHandler = (event) => {
+	event.preventDefault()
+
+	axios.post("http://localhost:8000/api/rocks/create", form)
+	.then(res => {
+		console.log(res);
+		setLoaded(false);
+	})
+	.catch(err => console.log(err))
+}
+
+
+	return (
     <div className="App">
-      <h1>Full stack time!</h1>
-          <table className="table table-dark">
-            <thead>
-              <tr>
+    <h1>Full stack time!</h1>
+        
+        <form className="m-5" onSubmit={onSubmitHandler}>
+        
+        <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" className="form-control" onChange={onChangeHandler} />
+        </div>
+
+        <div className="form-group">
+        <label htmlFor="color">Color:</label>
+        <input type="text" name="color" className="form-control" onChange={onChangeHandler}/>
+        </div>
+
+        <div className="form-group">
+        <label htmlFor="feel">Feel:</label>
+        <input type="text" name="feel" className="form-control" onChange={onChangeHandler}/>
+        </div>
+
+        <div className="form-group">
+        <label htmlFor="weight">Weight:</label>
+        <input type="text" name="weight" className="form-control" onChange={onChangeHandler}/>
+        </div>
+        <input type="submit" value="Add rock" className="btn btn-success" />
+        </form>
+        <table className="table table-dark">
+        <thead>
+            <tr>
                 <th>Name</th>
                 <th>Color</th>
                 <th>Feel</th>
                 <th>Weight</th>
                 <th>Date Found</th>
-              </tr>
+            </tr>
             </thead>
             <tbody>
             {
-              rocks.map((item, i) => {
-                return <tr key={i}>
-                  <td className="table-light">{item.name}</td>
-                  <td className="table-light">{item.color}</td>
-                  <td className="table-light">{item.feel}</td>
-                  <td className="table-light">{item.weight}</td>
-                  <td className="table-light">{item.foundDate}</td>
-                </tr>
-              })
-            }
-            </tbody>
+            rocks.map((item, i) => {
+            return <tr key={i}>
+            <td className="table-light">{item.name}</td>
+            <td className="table-light">{item.color}</td>
+            <td className="table-light">{item.feel}</td>
+            <td className="table-light">{item.weight}</td>
+            <td className="table-light">{item.foundDate}</td>
+            </tr>
+            })
+        }
+        </tbody>
         </table>
     </div>
-  );
+	);
 }
 
 export default App;
--- MERN/rock_project/server/controllers/rock.controller.js
+++ MERN/rock_project/server/controllers/rock.controller.js
@@ -23,6 +23,7 @@ module.exports.updateRock = (req, res) => {
     .then(rock => {
         rock.name = req.body.name,
         rock.color = req.body.color,
+        rock.feel = req.body.feel,
         rock.weight = req.body.weight,
         rock.foundDate = req.body.foundDate
         return rock.save();
@@ -35,4 +36,4 @@ module.exports.deleteRock = (req, res) => {
     Rock.deleteOne({ _id: req.params.id })
         .then(result => res.json({message: "Rock deleted", result: result }))
         .catch(err => res.json({ message: 'Not quite right!', error: err }));
-    }
+}
