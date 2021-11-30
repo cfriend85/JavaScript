cba08d09bc163ee9c5cc26e0a486edf0fefe80f9
Charles <c.friend85@yahoo.com>
2021-11-30 11:16:45 -0700

    Starting 30 days of Code on HackerRank

---
+++ 30_Days_of_Code/Day0_hello_world.js
@@ -0,0 +1,15 @@
+// function processData(inputString) {
+//     // This line of code prints the first line of output
+//     console.log("Hello, World.");
+//     let myString = inputString
+//     // Write the second line of output that prints the contents of 'inputString' here.
+//     console.log(myString);
+// }
+
+
+const processData = (inputString) => {
+    let myString = inputString
+    console.log('Hello, World.', myString)
+}
+
+processData('Welcome to 30 days of code!')
