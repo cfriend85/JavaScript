eb6d4153ff7bab7a305e8eb7c1a3e0b07c791770
Charles <c.friend85@yahoo.com>
2021-12-01 01:04:28 -0700

    Finish day 7

--- HR_JS_challenges/day7.js
+++ HR_JS_challenges/day7.js
@@ -5,3 +5,13 @@
 const getMaxLessThanK = (n, k) => {
     return ((k | (k - 1)) > n) ? (k - 2) : (k - 1);
 }
+
+
+// Given a date string, dateString, in the format MM/DD/YYYY, find and return the day name for that date. 
+// Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. 
+// For example, the day name for the date 12/07/2016 is Wednesday.
+
+const getDayName = (dateString) => {
+    let dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    return dayNames[new Date(dateString).getDay()]
+}
